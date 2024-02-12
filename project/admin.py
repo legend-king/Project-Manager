93d0225a4a9585c541d0987606f3dfafe2d7e3bd
Mustafa Abdullahbhai Khorakiwala <mustafakhorakiwala53@gmail.com>
2024-02-12 22:12:07 +0530

    Initial commit

---
+++ project/admin.py
@@ -0,0 +1,62 @@
+from django.contrib import admin
+from adminsortable2.admin import SortableAdminMixin, SortableStackedInline, SortableAdminBase
+from .models import Project, ProjectModule, ModuleTask
+# Register your models here.
+
+class ModuleTaskInline(admin.StackedInline):
+    model = ModuleTask
+    extra = 1
+
+class ProjectModuleInline(admin.TabularInline):
+    model = ProjectModule
+    extra = 1
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'priority', 'status','due_date']
+    search_fields = ['title', 'description']
+    list_editable = ['priority', 'status']
+    list_filter = ['priority', 'status', 'due_date']
+    fields = ['title', 'description','priority', 'status', 'due_date']
+    inlines = [ProjectModuleInline]
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            return qs.filter(user=request.user)
+        return qs
+    
+
+
+@admin.register(ProjectModule)
+class ProjectModuleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'project', 'priority', 'completed', 'due_date']
+    search_fields = ['title', 'description', 'project__title']
+    list_filter = ['project', 'priority', 'completed']
+    list_editable = ['priority', 'completed']
+    autocomplete_fields = ['project']
+    inlines = [ModuleTaskInline]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            return qs.filter(project__user=request.user)
+        return qs
+
+@admin.register(ModuleTask)
+class ModuleTaskAdmin(SortableAdminMixin, admin.ModelAdmin):
+    ordering = ['my_order']
+    list_display = ['title', 'module', 'completed', 'due_date', 'my_order']
+    search_fields = ['title']
+    list_filter = ['module', 'completed']
+    list_editable = ['completed']
+    autocomplete_fields = ['module']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            return qs.filter(module__project__user=request.user)
+        return qs
---
+++ project/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+    priority = models.IntegerField(choices=[(1, "Low"), (2,"Medium"), (3, "High")])
+    status = models.IntegerField(choices=[(1,"Not yet Started"), (2, "Work In Progress"),
+                                           (3, "Live but modules pending"), (4, "All Modules Completed")], default=1)
+    due_date = models.DateField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
+    
+    class Meta:
+        ordering = ('status', 'priority', '-due_date')
+
+class ProjectModule(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    priority = models.IntegerField(choices=[(1, "Low"), (2,"Medium"), (3, "High")], default=3)
+    completed = models.BooleanField(default=False)
+    due_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        unique_together = ('project', 'title')
+        ordering = ('completed', 'priority', '-due_date')
+
+    def __str__(self):
+        return f"{self.project} - {self.title}"
+    
+class ModuleTask(models.Model):
+    title = models.CharField(max_length=255)
+    module = models.ForeignKey(ProjectModule, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+    due_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    my_order = models.PositiveIntegerField(
+        default=0,
+        blank=False,
+        null=False,
+    )
+
+    class Meta:
+        ordering = ['my_order']
+
+    def __str__(self):
+        return f"{self.module} - {self.title}"
